0a218809e3af1c575d04bea2b39720bf15b7845d
Magdalena Petrova <magik70@abv.bg>
2015-03-11 08:06:47 +0200

    JavaScriptAdvanced_Homework

---
+++ JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/Homework_FunctionsAndFunctionExpressions/Homework_FunctionsAndFunctionExpressions/01_functionArguments/01_functionArguments.js
@@ -0,0 +1,12 @@
+﻿function printArgsInfo(){
+    
+    for (var i in arguments) {
+        var type = typeof arguments[i];
+        console.log(arguments[i] + " " + "(" + type + ")");
+    }
+}
+printArgsInfo(2, 3, 2.5, -110.5564, false);
+printArgsInfo(null, undefined, "", 0, [], {});
+//printArgsInfo([1, 2], ["string", "array"], ["single value"]);
+//printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+//printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
---
+++ JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/Homework_FunctionsAndFunctionExpressions/Homework_FunctionsAndFunctionExpressions/03_TestValueOf_this/03_TestValueOf_this.js
@@ -0,0 +1,19 @@
+﻿function testContext() {
+    console.log(this);
+}
+
+console.log("'--------------------");
+console.log("From the global scope ->");
+testContext();
+
+console.log("'--------------------");
+console.log("Inside another function ->");
+fromFunction();
+
+function fromFunction() {
+    testContext();
+}
+
+console.log("'--------------------");
+console.log("As an object ->");
+var asObj = new testContext();
