c34971173ead5718650a13c9980273f02d26855d
Magdalena Petrova <magik70@abv.bg>
2015-03-11 09:54:58 +0200

    javaScriptAdvanced_Homework

--- JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/04_AddingNumbersUsingFunctions/04_AddingNumbersUsingFunctions.js
+++ JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/04_AddingNumbersUsingFunctions/04_AddingNumbersUsingFunctions.js
@@ -14,9 +14,10 @@ console.log(+add(1)); // returns 1
 console.log(+add(2)(3)); // returns 5
 console.log(+add(1)(1)(1)(1)(1)); // returns 5
 console.log(+add(1)(0)(-1)(-1)); // returns -1
+
 console.log();
+
 var addTwo = add(2);
 console.log(+addTwo); // 2
-var addTwo = add(2);
 console.log(+addTwo(3)); // 5
 console.log(+addTwo(3)(5)); //10
