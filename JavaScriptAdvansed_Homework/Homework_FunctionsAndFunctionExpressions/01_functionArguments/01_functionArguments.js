6d52d2d3ea8a167053d7ede787e31d3ac7d6e529
Magdalena Petrova <magik70@abv.bg>
2015-03-12 17:53:57 +0200

    JavaScriptAdvanced_Homework

--- JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/01_functionArguments/01_functionArguments.js
+++ JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/01_functionArguments/01_functionArguments.js
@@ -5,8 +5,17 @@
         console.log(arguments[i] + " " + "(" + type + ")");
     }
 };
-printArgsInfo(2, 3, 2.5, -110.5564, false);
-printArgsInfo(null, undefined, "", 0, [], {});
-//printArgsInfo([1, 2], ["string", "array"], ["single value"]);
-//printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+//printArgsInfo(2, 3, 2.5, -110.5564, false);
+//printArgsInfo(null, undefined, "", 0, [], {});
+
+// use apply()
+printArgsInfo([1, 2], ["string", "array"], ["single value"]);
+printArgsInfo.apply([1, 2], ["string", "array"], ["single value"]);
+printArgsInfo.apply();
+
+// use call()
+printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+printArgsInfo.call("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+printArgsInfo.call();
+
 //printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
