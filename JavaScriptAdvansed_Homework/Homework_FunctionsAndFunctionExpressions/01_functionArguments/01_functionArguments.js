7b5f943e0e825904178bf91ff9964a4c01a0b791
Magdalena Petrova <magik70@abv.bg>
2015-03-13 17:32:00 +0200

    fix apply and call

--- JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/01_functionArguments/01_functionArguments.js
+++ JavaScriptAdvansed_Homework/Homework_FunctionsAndFunctionExpressions/01_functionArguments/01_functionArguments.js
@@ -10,12 +10,12 @@
 
 // use apply()
 printArgsInfo([1, 2], ["string", "array"], ["single value"]);
-printArgsInfo.apply([1, 2], ["string", "array"], ["single value"]);
+printArgsInfo.apply(null, [[1, 2], ["string", "array"], ["single value"]]);
 printArgsInfo.apply();
 
 // use call()
 printArgsInfo("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
-printArgsInfo.call("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
+printArgsInfo.call(null, "some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], { name: "Peter", age: 20 });
 printArgsInfo.call();
 
 //printArgsInfo([[1, [2, [3, [4, 5]]]], ["string", "array"]]);
