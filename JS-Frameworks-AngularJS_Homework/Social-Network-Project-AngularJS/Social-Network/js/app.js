1170f31e2aa84b48249393e0641c65f8b330964c
Magdalena Petrova <magik70@abv.bg>
2015-05-17 11:48:32 +0300

    add Start Page, Login Form and Register Form templates

--- JS-Frameworks-AngularJS_Homework/Social-Network-Project-AngularJS/Social-Network/js/app.js
+++ JS-Frameworks-AngularJS_Homework/Social-Network-Project-AngularJS/Social-Network/js/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var app = angular.module('socialNetworkApp', []);
+var app = angular.module('socialNetworkApp', ['ngResource', 'ngRoute'])
+	.config(function ($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'templates/startPage.html',
+			controller: 'SocialNetworkController'
+		});
+		$routeProvider.when('/login', {
+			test: 'Login',
+			templateUrl: 'templates/loginForm.html',
+			controller: 'SocialNetworkController'
+		});
+		$routeProvider.when('/register', {
+			test: 'Register',
+			templateUrl: 'templates/registerForm.html',
+			controller: 'SocialNetworkController'
+		});
+	});
