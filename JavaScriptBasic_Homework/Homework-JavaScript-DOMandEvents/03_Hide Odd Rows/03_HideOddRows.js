097115e0074d1e8ce53f033636f0d0b851168f3e
Magdalena Petrova <magik70@abv.bg>
2017-05-27 20:07:00 +0300

    :)

--- JavaScriptBasic_Homework/Homework-JavaScript-DOMandEvents/03_Hide Odd Rows/03_HideOddRows.js
+++
@@ -1,9 +0,0 @@
-function hideOddRows() {
-    var row = document.getElementsByTagName('tr');
-
-    for (var i = 0; i < row.length; i += 2) {
-            row[i].style.display = 'none';
-    }
-}
-
-document.getElementById('btnHideOddRows').addEventListener('click', hideOddRows);
